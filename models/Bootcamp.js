37cde1e8a77886a37ba86462557422649955a15c
Tereza Simkova <simkovat@gmail.com>
2019-11-25 16:19:50 +0100

    Bootcamp & User Relationship

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -98,6 +98,11 @@ const BootcampSchema = new mongoose.Schema(
     createdAt: {
       type: Date,
       default: Date.now
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User', // reference to the User model
+      require: true
     }
   },
   {
--- seeder.js
+++ seeder.js
@@ -12,6 +12,7 @@ dotenv.config({ path: './config/config.env' });
 // Load modules
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
+const User = require('./models/User');
 
 // Connect to DB
 mongoose.connect(process.env.MONGO_URI, {
@@ -30,11 +31,16 @@ const courses = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
 );
 
+const users = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
+);
+
 // Import into DB
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
+    await User.create(users);
 
     console.log('Data Imported...'.green.inverse);
     process.exit();
@@ -48,6 +54,7 @@ const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
     await Course.deleteMany();
+    await User.deleteMany();
 
     console.log('Data Destroyed...'.red.inverse);
     process.exit();
