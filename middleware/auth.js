06726281055244e5ece7beb39d3495ea29ed1432
Tereza Simkova <simkovat@gmail.com>
2019-12-23 13:35:24 +0100

    Logout To Clear Token Cookie

--- middleware/auth.js
+++ middleware/auth.js
@@ -11,12 +11,13 @@ exports.protect = asyncHndler(async (req, res, next) => {
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
+    // Set token from bearer token in header
     token = req.headers.authorization.split(' ')[1];
+    // Set token from cookie
   }
-
-  //   else if (req.cookies.token) {
-  //     token = req.cookies.token;
-  //   }
+  // else if (req.cookies.token) {
+  //   token = req.cookies.token;
+  // }
 
   // Make sure token exists
   if (!token) {
