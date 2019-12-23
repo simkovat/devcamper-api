30bb6d4f70afd29ddb1f06509671cf229339cac7
Tereza Simkova <simkovat@gmail.com>
2019-12-23 14:18:05 +0100

    Rate Limiting, HPP & CORS

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const fileUpload = require('express-fileupload');
 const cookieParser = require('cookie-parser');
 const mongoSanitize = require('express-mongo-sanitize');
 const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const hpp = require('hpp');
+const cors = require('cors');
+
 const xss = require('xss-clean');
 
 // Load env vars
@@ -48,6 +52,20 @@ app.use(helmet());
 
 // Prevent XSS attacks
 app.use(xss());
+
+// Rate limiting
+const limiter = rateLimit({
+  windowMs: 10 * 60 * 1000, // 10 mins
+  max: 100
+});
+
+app.use(limiter);
+
+// Prevent http param pollution
+app.use(hpp());
+
+// Enable CORS
+app.use(cors());
 
 // Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
